ab7c3e863cce54269dc5e1bb7437bc107b87dba9
Victor Lavrentev <victor.lavrentev@gmail.com>
2014-03-25 12:06:42 +0200

    RGB gamma
    RGB gamma (lin-rgb)

--- exr_view.py
+++ exr_view.py
@@ -1,27 +1,49 @@
 import OpenEXR
 import Imath
-import Image
-import sys
+from PIL import Image
+import sys, array
 import os
 
-exrfile=sys.argv[1]
-jpgfile='exr.jpg'
-file = OpenEXR.InputFile(exrfile)
-pt = Imath.PixelType(Imath.PixelType.FLOAT)
-dw = file.header()['dataWindow']
-size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+exrf=sys.argv[1]
 
-rgbf = [Image.fromstring("F", size, file.channel(c, pt)) for c in "RGB"]
+def exrToJpgGamma(exrfile):
+    file = OpenEXR.InputFile(exrfile)
+    pt = Imath.PixelType(Imath.PixelType.FLOAT)
+    dw = file.header()['dataWindow']
+    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
 
-extrema = [im.getextrema() for im in rgbf]
-darkest = min([lo for (lo,hi) in extrema])
-lighest = max([hi for (lo,hi) in extrema])
-scale = 255 / (lighest - darkest)
-def normalize_0_255(v):
-    return (v * scale) + darkest
-rgb8 = [im.point(normalize_0_255).convert("L") for im in rgbf]
-Image.merge("RGB", rgb8).save(jpgfile)
+    RedStr = file.channel('R', pt)
+    GreenStr = file.channel('G', pt)
+    BlueStr = file.channel('B', pt)
 
-    
-ix=Image.open('exr.jpg').show()
-os.remove('exr.jpg')
+    Red = array.array('f', RedStr)
+    Green = array.array('f', GreenStr)
+    Blue = array.array('f', BlueStr)
+
+    def EncodeToSRGB(v):
+        if (v <= 0.0031308):
+            return (v * 12.92) * 255.0
+        else:
+            return (1.055*(v**(1.0/2.2))-0.055) * 255.0
+
+    for I in range(len(Red)):
+        Red[I] = EncodeToSRGB(Red[I])
+
+    for I in range(len(Green)):
+        Green[I] = EncodeToSRGB(Green[I])
+
+    for I in range(len(Blue)):
+        Blue[I] = EncodeToSRGB(Blue[I])
+
+    rgbf = [Image.fromstring("F", size, Red.tostring())]
+    rgbf.append(Image.fromstring("F", size, Green.tostring()))
+    rgbf.append(Image.fromstring("F", size, Blue.tostring()))
+
+    rgb8 = [im.convert("L") for im in rgbf]
+    myqimage = Image.merge("RGB", rgb8)
+    return myqimage.show()
+
+exrToJpgGamma(exrf)
+
+
+
